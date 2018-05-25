500f5c61d6e71d4f1cf325e0440ccbbaea56a1af
airzhang23 <air.zsl@gmail.com>
2018-05-25 17:19:12 +0800

    new

--- post.sh
+++ post.sh
@@ -7,7 +7,7 @@ echo "git committing..."
 echo ""
 git add .
 git commit -m "$1"
-git push -u origin master
+#git push -u origin master
 git push -u origin gh-pages
 echo ""
 echo "------ git status:------"
